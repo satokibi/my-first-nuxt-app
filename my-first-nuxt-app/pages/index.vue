648ca3259d845edf847767d4d0da1694999637d2
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-27 11:43:06 +0900

    add nuxtjs/axios

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -5,19 +5,6 @@
       <h1 class="title">
         Hello, World!
       </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
     </div>
   </section>
 </template>
@@ -28,6 +15,13 @@ import AppLogo from '~/components/AppLogo.vue'
 export default {
   components: {
     AppLogo
+  },
+  async mounted() {
+    console.log(
+      JSON.stringify(await
+      this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js', true, '  ')
+      )
+    )
   }
 }
 </script>
