52ad9179286b87756fb5aba5ed80ace4d8d4998f
satokibi <td182007@cis.fukuoka-u.ac.jp>
2019-01-29 10:51:20 +0900

    finish

--- my-first-nuxt-app/pages/index.vue
+++ my-first-nuxt-app/pages/index.vue
@@ -6,7 +6,12 @@
         <li v-for="item in items" :key="item.id">
           <h4>
             <span>{{ item.title }}</span>
-            <small> by {{ item.user.id }}</small>
+            <small>
+              <span>by </span>
+              <nuxt-link :to="`/users/${item.user.id}`">
+                {{ item.user.id }}
+              </nuxt-link>
+              </small>
           </h4>
           <div>{{ item.body.slice(0, 130) }}....</div>
           <p><a :href="item.url">{{ item.url }}</a></p>
@@ -17,16 +22,18 @@
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
+import { mapGetters } from 'vuex';
 export default {
-  async asyncData({ app }) {
-    const items = await app.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js')
-    return {
-      items
+  async asyncData({ store }) {
+    if (store.getters['items'].length) {
+      return;
     }
+    await store.dispatch('fetchItems');
+  },
+  computed: {
+    ...mapGetters(['items'])
   }
-}
+};
 </script>
 
 <style>
